08f4439f9f8ceb5ec2ebc9805f2e8c8e82f12071
Eunjee <58850132+wowww@users.noreply.github.com>
2021-12-20 14:59:04 +0900

    feat. Particles 100개 화면에 랜덤으로 출력
    this.speedX = Math.random() * 3 - 1.5;
    this.speedY = Math.random() * 3 - 1.5;
    이 값으로 랜덤으로 상하 좌우로 이동함

--- canvasBasic/js/script.js
+++ canvasBasic/js/script.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const particlesArray = [];
 
 window.addEventListener('resize', function() {
   canvas.width = window.innerWidth;
@@ -25,18 +26,13 @@ canvas.addEventListener('mousemove', function(event) {
 
 })
 
-function drawCircle() {
-  ctx.fillStyle = 'blue';
-  ctx.lineWidth = 5;
-  ctx.beginPath();
-  ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2)
-  ctx.fill();
-}
-
 class Particle {
   constructor() {
-    this.x = mouse.x;
-    this.y = mouse.y;
+    // this.x = mouse.x;
+    // this.y = mouse.y;
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+
     this.size = Math.random() * 5 + 1;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
@@ -46,11 +42,34 @@ class Particle {
     this.x += this.speedX;
     this.y += this.speedY;
   }
+
+  draw() {
+    ctx.fillStyle = 'blue';
+    ctx.lineWidth = 5;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 50, 0, Math.PI * 2)
+    ctx.fill();
+  }
+}
+
+function init() {
+  for (let i = 0; i < 100; i++) {
+    particlesArray.push(new Particle());
+  }
+}
+
+init();
+
+function handleParticles() {
+ for (let i = 0; i < particlesArray.length; i++) {
+  particlesArray[i].update();
+  particlesArray[i].draw();
+ }
 }
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawCircle();
+  handleParticles();
   requestAnimationFrame(animate);
 }
 
