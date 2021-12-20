22b3578efa219ba8542a249aa8b03e25967ffa8d
Eunjee <58850132+wowww@users.noreply.github.com>
2021-12-20 15:06:36 +0900

    feat. size 랜덤 값

--- canvasBasic/js/script.js
+++ canvasBasic/js/script.js
@@ -33,7 +33,7 @@ class Particle {
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
 
-    this.size = Math.random() * 5 + 1;
+    this.size = Math.random() * 15 + 1;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
   }
@@ -47,7 +47,7 @@ class Particle {
     ctx.fillStyle = 'blue';
     ctx.lineWidth = 5;
     ctx.beginPath();
-    ctx.arc(this.x, this.y, 50, 0, Math.PI * 2)
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
     ctx.fill();
   }
 }
