967ab945e23cfda3931b75a14ff00d7d66c8a13b
Eunjee <58850132+wowww@users.noreply.github.com>
2021-12-20 15:11:58 +0900

    feat. particlesArray splice
    - particlesArray splice 할 때 i--를 넣어주지 않으면 사라질 때마다 깜빡 깜빡 거림

--- canvasBasic/js/script.js
+++ canvasBasic/js/script.js
@@ -66,6 +66,11 @@ function handleParticles() {
  for (let i = 0; i < particlesArray.length; i++) {
   particlesArray[i].update();
   particlesArray[i].draw();
+
+  if ( particlesArray[i].size <= 0.3 ) {
+    particlesArray.splice(i, 1)
+    i--;
+  } 
  }
 }
 
