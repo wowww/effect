372a9de719a3cd14022aea886f1924b271ffa44e
Eunjee <58850132+wowww@users.noreply.github.com>
2021-12-20 15:16:52 +0900

    feat. mouse click, mouse move 이벤트에 파티클 적용

--- canvasBasic/js/script.js
+++ canvasBasic/js/script.js
@@ -17,21 +17,26 @@ const mouse = {
 canvas.addEventListener('click', function(event) {
   mouse.x = event.x;
   mouse.y = event.y;
+  for (let i = 0; i < 10; i++) {
+    particlesArray.push(new Particle());
+  }
 
 })
 
 canvas.addEventListener('mousemove', function(event) {
   mouse.x = event.x;
   mouse.y = event.y;
-
+  for (let i = 0; i < 2; i++) {
+    particlesArray.push(new Particle());
+  }
 })
 
 class Particle {
   constructor() {
-    // this.x = mouse.x;
-    // this.y = mouse.y;
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
+    this.x = mouse.x;
+    this.y = mouse.y;
+    // this.x = Math.random() * canvas.width;
+    // this.y = Math.random() * canvas.height;
 
     this.size = Math.random() * 15 + 1;
     this.speedX = Math.random() * 3 - 1.5;
@@ -53,14 +58,6 @@ class Particle {
     ctx.fill();
   }
 }
-
-function init() {
-  for (let i = 0; i < 100; i++) {
-    particlesArray.push(new Particle());
-  }
-}
-
-init();
 
 function handleParticles() {
  for (let i = 0; i < particlesArray.length; i++) {
