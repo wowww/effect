13a2453fbe00541a7115326b8801a8b055646415
Eunjee <58850132+wowww@users.noreply.github.com>
2021-12-20 14:24:26 +0900

    feat. 클릭 이벤트로 원 그리기

---
+++ canvasBasic/js/script.js
@@ -0,0 +1,34 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener('resize', function() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight; 
+})
+
+const mouse = {
+  x: null,
+  y: null,
+}
+
+canvas.addEventListener('click', function(event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  console.log(event)
+
+  drawCircle();
+})
+
+function drawCircle() {
+  ctx.fillStyle = 'blue';
+  ctx.strokeStyle = 'red';
+  ctx.lineWidth = 5;
+  ctx.beginPath();
+  ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2)
+  ctx.fill();
+  ctx.stroke();
+}
+
+drawCircle();
